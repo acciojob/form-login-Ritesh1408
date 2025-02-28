741996242679e09fa5087d5f0f5f1133014d6172
RITESH ROUSHAN KUMAR <78962046+Ritesh1408@users.noreply.github.com>
2025-02-28 12:47:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-function getFormvalue() {
-    //Write your code here
+document.getElementById('form1').onsubmit = function (event) {
+    event.preventDefault(); 
+    getFormvalue(); 
+};
 
+function getFormvalue() {
+    let firstName = document.querySelector('input[name="fname"]').value.trim();
+    let lastName = document.querySelector('input[name="lname"]').value.trim();
+
+    let fullName = `${firstName} ${lastName}`;
+    
+    if (fullName.trim() !== "") {
+        alert(fullName);
+    } else {
+        alert("Please enter your first and last name.");
+    }
 }
